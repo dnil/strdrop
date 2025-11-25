bd5481e040c0de1fdd9d3949af93c0cf8c614ffa
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2025-11-25 18:03:05 +0100

    test some norm

--- strdrop/cli/main.py
+++ strdrop/cli/main.py
@@ -36,8 +36,8 @@ app = typer.Typer(
     help="Call coverage drops over alleles in STR VCFs"
 )
 
-def parse_sds(file: Path, training_data:dict = {}):
-
+def parse_sds(file: Path, training_data:dict = {}) -> bool:
+    """Parse SDs from VCF. Return False if file was not found."""
     if os.path.isfile(file):
         training_vcf = VCF(file)
         for variant in training_vcf:
@@ -55,8 +55,9 @@ def parse_sds(file: Path, training_data:dict = {}):
                 # training_data[trid].append(ref_value)
                 training_data[trid].append(ref_sd + alt_sd)
                 continue
-            training_data[trid] = [alt_sd]
-
+            training_data[trid] = [ref_sd + alt_sd]
+        return True
+    return False
 
 
 @app.callback()
@@ -83,21 +84,39 @@ def call(training_set: Annotated[Path,
 
     training_data = {}
 
+    n_training_cases = 0
     for training_file in os.listdir(training_set):
         tf = os.path.join(training_set, training_file)
-        parse_sds(tf, training_data)
-
+        if parse_sds(tf, training_data):
+            n_training_cases = n_training_cases + 1
 
     test_data = {}
     parse_sds(input_file, test_data)
 
+    alpha = 0.05
+    p_threshold = alpha / len(test_data.keys())
+
+    # plain per allele counts
     for trid in test_data.keys():
         td = pd.Series(sorted(training_data[trid]))
         count_value = (td[td<test_data[trid][0]]).sum()
         total_value = td.sum()
         p = count_value / total_value if total_value > 0 else 0
-        if(p < 0.05):
-            print(f"{trid} low with {test_data[trid][0]} (P={p})")
+        if(p < p_threshold):
+            print(f"{trid} locus overall low with {test_data[trid][0]} (P={p})")
+
+    # per allele counts normalised with total case count
+    total_sum_training = 0
+    for trid in test_data.keys():
+        td = pd.Series(sorted(training_data[trid]))
+        total_sum_training += td.sum()
+
+    average_count_per_case = total_sum_training / n_training_cases
+    for trid in test_data.keys():
+        td = pd.Series(sorted(training_data[trid]))
+        count_norm = (td[td<test_data[trid][0]]).sum()
+
+
 
 def run():
     app()
